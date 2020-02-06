61fab1e48ab5cc9cf3f9c23b0392788c47287a99
CRD1998 <60524724+CRD1998@users.noreply.github.com>
2020-02-06 15:35:46 +0000

    Add files via upload
    How to read fits files with astropy

---
+++ Our Scripts/readFits.py
@@ -0,0 +1,14 @@
+"""
+This script shows how you can open and extract data from a fits file in python, you need astropy
+which is included in the anaconda distribution. The fits files for lyman alpha luminsoity are on
+the box.
+"""
+from astropy.io import fits
+from astropy.table import Table
+
+filename = 'Sobral2018_Integrals_SFRDs_individual.fits' # the file you want to open
+hdu_list = fits.open(filename) # open the file, returns an array containing raw data and metadata
+evt_data = Table(hdu_list[1].data) # The raw data is contained in the first element
+rho_Lya = evt_data.field(3)[0:] # get a list of the all the data from fourth field
+for i in rho_Lya:
+    print(i)
