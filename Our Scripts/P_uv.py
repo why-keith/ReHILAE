450467b53e3389a0d432b31a7b433c313887cc21
JP-Carr <j.p.carr@lancaster.ac.uk>
2020-02-13 10:39:12 +0000

    Update P_uv.py

--- Our Scripts/P_uv.py
+++ Our Scripts/P_uv.py
@@ -5,13 +5,16 @@ import plotter
 import math
 from Redshift import redshift as z
 
-startTime=0.01
-finishTime=14
-timeStep = 0.01
+#TIME VALUES
+startTime = mod.startTime
+finishTime = mod.finishTime
+timeStep = mod.timeStep
 
+#GENERATE Z AND t ARRAYS
 Z,t = z(startTime, finishTime, timeStep)
-#print (z)
-P_uv= np.array([mod.P_uv(i) for i in Z], dtype=np.float64)
-#print (t_rec)
 
-plotter.plot(Z,P_uv,"A plot of redshift against UV density","z","P_uv")
+#GENERATE P_uv ARRAY
+P_uv=[mod.P_uv(i) for i in Z]
+
+#PLOT Z AGAINST P_uv
+plotter.plot(Z,P_uv,"A plot of redshift against UV density","z","log(P_uv)")
