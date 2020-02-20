fbf7a80e194793825b6993fdbb87cb2f4b24259f
Meckuts <60736623+Meckuts@users.noreply.github.com>
2020-02-20 15:35:34 +0000

    Add files via upload

--- Our Scripts/plotter.py
+++ Our Scripts/plotter.py
@@ -2,13 +2,14 @@
 Provides functions to produce graphs with single and multiple plots
 """
 import matplotlib.pyplot as plt
+import ErrorFits
 #import numpy as np
 #import MPhys_model.py as mod
 
 #path=None
 
 #PLOTS PARTICLE POSITION AS STATIC GRAPH
-def plot(x,y,title,x_lab,y_lab,path=None):
+def plot(x,y,title,x_lab,y_lab,path=None,error_list):
     #Outputs or saves a single Cartesian plot from 2 arrays
     plt.figure(figsize=(7,7))
     """
@@ -21,17 +22,19 @@ def plot(x,y,title,x_lab,y_lab,path=None):
     plt.xlabel(x_lab)
     plt.ylabel(y_lab)
     plt.plot(x,y)
+    
+    min_list, max_list = ErrorFits.error_Range(x,y,error_list[1],error_list[2])
+    plt.fill_between(x,min_list,max_list,lw=1,color='#0066ff',alpha=0.1,zorder = 90)
+  
     if path==None:
         plt.show()
     else:
         plt.savefig("plots\\"+path+"\\"+title+".png")
     
 
-def multiplot(x_list,y_list,title,x_lab,y_lab,legend_list=None,path=None,marker_list=None):
+def multiplot(x_list,y_list,title,x_lab,y_lab,legend_list=None,path=None):
     #Outputs or saves a multiple plots on a single Cartesian axis from 2 nested arrays
     max_x=max([max(i) for i in x_list])
-    if marker_list==None:
-        marker_list=[None for i in range(len(x_list))]
     
     plt.figure(figsize=(7,7))
     plt.xlim(left=0)
@@ -40,10 +43,7 @@ def multiplot(x_list,y_list,title,x_lab,y_lab,legend_list=None,path=None,marker_
     plt.xlabel(x_lab)
     plt.ylabel(y_lab)
     for i in range(len(x_list)):
-        if marker_list[i]!=None:
-            plt.plot(x_list[i], y_list[i],marker=marker_list[i],linewidth=0)
-        else:
-            plt.plot(x_list[i], y_list[i],marker=marker_list[i])
+        plt.plot(x_list[i], y_list[i])
     if legend_list != None:
         plt.legend(legend_list)
         
