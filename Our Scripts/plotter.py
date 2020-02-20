3fbacf3b5165d26a754601cb87acd9d9ceefa54b
JP-Carr <j.p.carr@lancaster.ac.uk>
2020-02-20 15:56:32 +0000

    Update plotter.py
    Added multiplot changes from master

--- Our Scripts/plotter.py
+++ Our Scripts/plotter.py
@@ -33,9 +33,11 @@ def plot(x,y,title,x_lab,y_lab,path=None,error_list=None):
         plt.savefig("plots\\"+path+"\\"+title+".png")
     
 
-def multiplot(x_list,y_list,title,x_lab,y_lab,legend_list=None,path=None):
+def multiplot(x_list,y_list,title,x_lab,y_lab,legend_list=None,path=None,marker_list=None):
     #Outputs or saves a multiple plots on a single Cartesian axis from 2 nested arrays
     max_x=max([max(i) for i in x_list])
+    if marker_list==None:
+        marker_list=[None for i in range(len(x_list))]
     
     plt.figure(figsize=(7,7))
     plt.xlim(left=0)
@@ -44,7 +46,10 @@ def multiplot(x_list,y_list,title,x_lab,y_lab,legend_list=None,path=None):
     plt.xlabel(x_lab)
     plt.ylabel(y_lab)
     for i in range(len(x_list)):
-        plt.plot(x_list[i], y_list[i])
+        if marker_list[i]!=None:
+            plt.plot(x_list[i], y_list[i],marker=marker_list[i],linewidth=0)
+        else:
+            plt.plot(x_list[i], y_list[i],marker=marker_list[i])
     if legend_list != None:
         plt.legend(legend_list)
         
