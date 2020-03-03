feb3e389b106f8c601a37a4ae89982d779788b05
JP-Carr <j.p.carr@lancaster.ac.uk>
2020-03-03 15:39:19 +0000

    Added print lines for testing

--- Our Scripts/Main_LAE.py
+++ Our Scripts/Main_LAE.py
@@ -20,7 +20,7 @@ c_ha=1.36E-12 #Recombination coefficient
 EW_avg=140.321828866 #Average equivalent width (angstroms)
 
 
-
+log_list=[0,0,0,0]
 
 P1 = -0.05
 P2 = 0.44
@@ -75,7 +75,9 @@ def P_L_Lya(x, P1, P2, P3):
     return 10**(P1*x**2 + P2*x +P3)
 
 
-def f_esc_LyC(x, f1, f2):
+def f_esc_LyC(x, f1, f2):  #//////////////////////////////////////////////////////////////////////////////////
+    print("f_esc")
+    log_list[0]+=1
     return f1*x + f2
 
 def Q_ion_LyC(z, P1, P2, P3, f1, f2): # replaces P_uv and E_ion	
@@ -83,18 +85,22 @@ def Q_ion_LyC(z, P1, P2, P3, f1, f2): # replaces P_uv and E_ion
     return P_L_Lya(z, P1, P2, P3) / ((c_ha*(1 - f_esc_LyC(EW, f1, f2)))*(0.042 * EW))
 
 
-def n_ion_dot_LyC(z, P1, P2, P3,  f1, f2): # replaces n_ion_dot using Q_ion_LyC	
+def n_ion_dot_LyC(z, P1, P2, P3,  f1, f2): # replaces n_ion_dot using Q_ion_LyC	 /////////////////////////////////////////////////
+    print("n_ion_dot")
+    log_list[1]+=1
     if Q_ion_LyC(z, P1, P2, P3,  f1, f2) * f_esc_LyC(EW, f1, f2) / (2.938e+73) <= 0 :
         return 0 
     else:
         return Q_ion_LyC(z, P1, P2, P3, f1, f2) * f_esc_LyC(EW, f1, f2) / (2.938e+73) # (2.938e+73) converts from Mpc^-3 to cm^-3  - full units s^-1 Mpc^-3
 
 
-def Q_Hii_dot(z, Q, P1, P2, P3, f1, f2): #s⁻¹	def Q_Hii_dot(z,Q_Hii): #s⁻¹
+def Q_Hii_dot(z, Q, P1, P2, P3, f1, f2): #s⁻¹	def Q_Hii_dot(z,Q_Hii): #s⁻¹    /////////////////////////////////////////
+    print("Q_Hii_dot")
+    log_list[2]+=1
+    # print((((n_ion_dot_LyC(z, P1, P2, P3, f1, f2)/n_H()) - (Q/t_rec(z)))*3.1536e+16))
     return (((n_ion_dot_LyC(z, P1, P2, P3, f1, f2)/n_H()) - (Q/t_rec(z)))*3.1536e+16) # conversion from Gyr^-1 to s^-1	    return (((n_ion_dot(z)/n_H()) - (Q_Hii/t_rec(z)))*3.1536e+16)  
 
-def dQ_dt(Q, t, P1, P2, P3, f1, f2):
-    
+def dQ_dt(Q, t, P1, P2, P3, f1, f2):    
     dQ_dt = Q_Hii_dot(red(t), Q, P1, P2, P3, f1, f2)
     return dQ_dt
 
@@ -104,6 +110,8 @@ def main(arguements):
     Q = odeint(dQ_dt, Q_Hii_zero, t, args=(arguements))
     Q[Q>1.0] = 1.0 # 100% HII
     Q[Q<0.0] = 0.0 # 100% HI
-
-    return Q
+    print("Q")
+    log_list[3]+=1
+    print("----------------------------")
+    return Q    #///////////////////////////////////////////////////////////////////////
 
