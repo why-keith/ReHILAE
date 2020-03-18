74a80562f72eca9dab58fc673cc0daa6474ff9cb
CRD1998 <60524724+CRD1998@users.noreply.github.com>
2020-03-18 14:05:48 +0000

    Update Fesc_Lyc.py

--- Our Scripts/ModelFunctions/Ingredients/Fesc_Lyc.py
+++ Our Scripts/ModelFunctions/Ingredients/Fesc_Lyc.py
@@ -7,66 +7,42 @@ import math
 def Function(x, a1, a2):
     return a1*x + a2
 
-xs = pylab.array([79, 129, 83, 98, 75, 29, 15, 4])                        
-ys = pylab.array([0.132, 0.074, 0.072, 0.058, 0.056, 0.045, 0.032, 0.01]) 
-ys_err = 0.2*ys                                                           
-p2 = pylab.polyfit(xs, ys, 1.0)
+x = pylab.array([2.5,2.8,2.9,3.1, 3.3, 3.7, 4.1, 4.5, 4.8, 5.0, 5.3, 5.8])
+y = np.array([117.134349876, 122.113769531, 172.679885864, 143.5936203, 149.371124268, 96.3648490905, 189.002449036, 181.127731323, 95.0448436864, 125.935153962, 143.343048096, 251.93561])
+y_err_up =pylab.array([186.35171, 160.96419, 321.31418, 382.54525, 258.18398, 70.23115, 763.57573, 223.72103, 100.98794, 195.63354, 159.8391, 620.82504])
+y_err_down =pylab.array([53.82247, 53.55149, 90.78461, 72.25881, 74.65443, 39.08451, 96.45726, 85.27828, 42.09321, 52.08989, 71.00167, 186.34499])
+y_err = [(y_err_up[i]-y_err_down[i])/2 for i in range(len(y_err_down))]
+p2 = np.polyfit(x, y, 1.0)
 p = pylab.poly1d(p2)
-params = p.coefficients                                                 
-pfit, pcov = curve_fit(Function, xs,ys,p0=params,sigma=ys_err)
-error = [] 
+Params = p.coefficients
+pfit, pcov = curve_fit(Function, x, y, p0=Params, sigma=y_err)
+a1,a2= pfit[0], pfit[1]
+
+error = []
 for i in range(len(pfit)):
   try:
     error.append(np.absolute(pcov[i][i])**0.5)
   except:
     error.append(0.00)
-BFP = pfit  
-err_BFP = np.array(error) 
+BFP = pfit
+err_BFP = np.array(error)
+
+def EquivalentWidth(x):
+    return 6.67298*x + 80.96867
+
+def Fesc(z,a1,a2):
+    a1*EquivalentWidth(z) + a2
 
 print ('Function: y = (%s+-%s)x  + (%s+-%s)' %(round(BFP[0],5),round(err_BFP[0],5),round(BFP[1],5),round(err_BFP[1],5)))
 
 a1, a2, = BFP[0], BFP[1]
 a1_err, a2_err = err_BFP[0], err_BFP[1]
-
-def EW(x):
-    return 6.67298*x + 80.96867
-
-def Fesc(z,a1,a2):
-    return a1*EW(z) + a2
-
 zs = list(np.linspace(0.0051,14,10000))
 scipy_fit = [Fesc(z,a1,a2) for z in zs]
 
-number_of_iterations = 100
-a1_list = np.random.normal(a1, a1_err*0.2, number_of_iterations)
-a2_list = np.random.normal(a2, a2_err*0.2, number_of_iterations)
+print(scipy_fit)
 
-all_runs = []
-for _ in a1_list:
-    all_runs.append([])
-
-for A,B,loop in zip(a1_list,a2_list,all_runs):
-    for z in zs:
-        parameter = np.random.randint(2)
-        if parameter == 0:
-            loop.append(Fesc(z,A,a2))
-        else:
-            loop.append(Fesc(z,a1,B))
-
-
-median, median_upper_percentile, median_lower_percentile = [],[],[]
-for z in zs:
-    ind = zs.index(z)
-    equiWidths = [i[ind] for i in all_runs]
-    median_lower_percentile.append(np.percentile(equiWidths,56))
-    median.append(np.median(equiWidths))
-    median_upper_percentile.append(np.percentile(equiWidths,34))
-
-plt.figure('Fesc_LyC')
-plt.plot(zs, scipy_fit, color='black', label='SciPy fit')
-plt.fill_between(zs,  median_lower_percentile, median_upper_percentile, alpha=0.4, color = "grey", edgecolor = "black", linewidth = 1.2, label=r'68% Confidence Interval')
-plt.plot(zs, median, "--", label='Median',color='blue')
-plt.xlabel('Redshift (z)')
-plt.ylabel(r'$f_{esc,LyC}$')
-plt.legend()
+plt.plot(zs, scipy_fit)
 plt.show()
+
+
